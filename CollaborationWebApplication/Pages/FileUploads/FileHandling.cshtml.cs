b522a6e98ce2602e2d879a506a6923ed422cabc4
Parker-Short <prkrshort@gmail.com>
2024-02-22 15:51:43 -0500

    some CSS and layout changes

--- CollaborationWebApplication/Pages/FileUploads/FileHandling.cshtml.cs
+++ CollaborationWebApplication/Pages/FileUploads/FileHandling.cshtml.cs
@@ -5,8 +5,35 @@ namespace CollaborationWebApplication.Pages.FileUploads
 {
     public class FileHandlingModel : PageModel
     {
+        [BindProperty]
+        public List<IFormFile> FileList { get; set; }
+
         public void OnGet()
         {
         }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var csvDataList = new List<string>();
+
+            foreach (var formFile in FileList)
+            {
+                if (formFile.Length > 0)
+                {
+                    using (var reader = new StreamReader(formFile.OpenReadStream()))
+                    {
+                        string content = await reader.ReadToEndAsync();
+                        csvDataList.Add(content);
+                    }
+                }
+            }
+
+            // Store the CSV data in TempData. If multiple files are uploaded, you might need to handle them separately.
+            TempData["CsvData"] = JsonConvert.SerializeObject(csvDataList);
+
+            return RedirectToPage("/Datasets/DatasetForm");
+        }
+
     }
+    
 }
