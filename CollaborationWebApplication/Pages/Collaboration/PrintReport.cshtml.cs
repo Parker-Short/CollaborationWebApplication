53f3f520e9d710c180e0ba86fc00e2c05d5a40fe
Parker-Short <prkrshort@gmail.com>
2024-02-29 12:44:33 -0500

    fixed logout button alignment in navbar created new print report page

---
+++ CollaborationWebApplication/Pages/Collaboration/PrintReport.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CollaborationWebApplication.Pages.Collaboration
+{
+    public class PrintReportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
